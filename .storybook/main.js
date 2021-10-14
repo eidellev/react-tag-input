a8dace547ff90bfd449a3890606a39ae2e2e9279
eidellev <levp22@gmail.com>
2021-10-14 13:15:35 +0300

    added storybook

---
+++ .storybook/main.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ]
+}
---
+++ src/TagInput.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface Props {
+  options?: string[];
+  value: string[];
+  className?: string;
+}
+
+export const TagInput : React.FunctionComponent<Props> = (props: Props) => {
+  return <div></div>
+}
