3429fcdebbdc181a157d9d333bf3580263c5a1ba
eidellev <levp22@gmail.com>
2021-10-26 18:19:30 +0300

    chore: additional project config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
+  },
 };
--- src/TagInput.spec.tsx
+++ src/TagInput.spec.tsx
@@ -1,7 +1,10 @@
+import React from 'react';
+import { render } from '@testing-library/react';
 import { TagInput } from './TagInput';
 
 describe('Tag input', () => {
-  it('Should pass', () => {
-    expect(true).toBe(true);
+  it('Should render without errors', () => {
+    // eslint-disable-next-line
+    render(<TagInput value={[]} onChange={() => {}} />);
   });
 });
